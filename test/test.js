d683a076c464f34596f037b5ddb9db6acac17e3b
mastito03 <mr.aladin03@gmail.com>
2016-10-27 18:02:37 +0700

    add callback test

--- test/test.js
+++ test/test.js
@@ -1,7 +1,27 @@
 var assert = require('assert')
 var render = require('../index.js')
 
-describe('#render()', function() {
+describe('#render() -> callback', function() {
+    it('should render delta', function(done) {
+      delta = {
+        ops: [{
+          insert: 'Hello',
+          attributes: {
+            italic: true
+          }
+        }, {
+          insert: ' world!!'
+        }]
+      }
+      html = "<p><em>Hello</em><span> world!!</span></p>"
+      render(delta, (err, output) => {
+        if (err) return done(err)
+        done(assert.equal(output, html))
+      })
+  })
+})
+
+describe('#render() -> promise', function() {
     it('should render delta', function(done) {
       delta = {
         ops: [{
