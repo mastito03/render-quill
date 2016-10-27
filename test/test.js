5986892488bd7fbba04340d2a456e377ab2b7fb8
mastito03 <mr.aladin03@gmail.com>
2016-10-27 18:21:34 +0700

    iya wes

--- test/test.js
+++ test/test.js
@@ -1,41 +1,31 @@
 var assert = require('assert')
 var render = require('../index.js')
 
+delta = {
+  ops: [{
+    insert: 'Hello',
+    attributes: {
+      italic: true
+    }
+  }, {
+    insert: ' world!!'
+  }]
+}
+html = "<p><em>Hello</em><span> world!!</span></p>"
+
 describe('#render() -> callback', function() {
-    it('should render delta', function(done) {
-      delta = {
-        ops: [{
-          insert: 'Hello',
-          attributes: {
-            italic: true
-          }
-        }, {
-          insert: ' world!!'
-        }]
-      }
-      html = "<p><em>Hello</em><span> world!!</span></p>"
-      render(delta, (err, output) => {
-        if (err) return done(err)
-        done(assert.equal(output, html))
-      })
+  it('should render delta', function(done) {
+    render(delta, (err, output) => {
+      if (err) return done(err)
+      done(assert.equal(output, html))
+    })
   })
 })
 
 describe('#render() -> promise', function() {
-    it('should render delta', function(done) {
-      delta = {
-        ops: [{
-          insert: 'Hello',
-          attributes: {
-            italic: true
-          }
-        }, {
-          insert: ' world!!'
-        }]
-      }
-      html = "<p><em>Hello</em><span> world!!</span></p>"
-      render(delta).then( output => {
-        done(assert.equal(output, html))
-      }).catch( error => { done(error) })
+  it('should render delta', function(done) {
+    render(delta).then( output => {
+      done(assert.equal(output, html))
+    }).catch( error => { done(error) })
   })
 })
